8155292de558525b3898f805b1ab15f57aa708cd
Alfred Liu <alfred.liu53@gmail.com>
2023-06-02 22:35:17 +1000

    Fixed up local storage and attempt at multi modal

--- public/script.js
+++ public/script.js
@@ -1,7 +1,5 @@
 // The following lines of code has been inspired by the provided youtube resurce in Week 13 module
 // The youtube link can be found here: https://youtu.be/ywtkJkxJsdg; "The New dialog HTML Element Changes Modals Forever" by Web Dev Simplified
-
-
 const openButton = document.querySelector("[open-modal]")
 const closeButton = document.querySelector("[close-modal]")
 const modal = document.querySelector("dialog")
@@ -19,7 +17,6 @@ closeButton.addEventListener("click", () => {
 // Timer function
 // The following timer function has been inspired by Sam David on Quora. 
 // Link can be found here: https://www.quora.com/How-do-I-create-a-simple-JavaScript-count-up-timer-with-a-reset-button-in-HTML
-
 const counter = document.querySelector("#second"); 
 const startButton = document.querySelector("#timerStartButton"); 
 const stopButton = document.querySelector("#timerStopButton"); 
@@ -53,7 +50,6 @@ function resetTimer(){
 // Calendar Function
 // The following code has been inspired by Freecodecamp
 // https://www.freecodecamp.org/news/javascript-get-current-date-todays-date-in-js/#:~:text=In%20JavaScript%2C%20we%20can%20easily,%2C%20time%2C%20and%20time%20zone.
-
 // Date
 const date = new Date();
 
@@ -71,23 +67,49 @@ time.innerHTML = currentTime;
 // Object here for storing workout.
 // Each workout object will store: Category, Exercise Name, Exercise Difficulty, Num of Reps and Repetition Duration
 // How this function works is that it grabs the value from each form element
-// Saves it into the workout object
+// Saves said element it into the workout object
 // Saves that workout object into array for access later
 
-var formInputValue = document.forms["workoutRepForm"]
-var a = document.getElementById("exerciseCategory").value;
-var b = document.getElementById("exerciseName").value;
-var c = document.getElementById("exerciseDifficulty").value;
-var d = document.getElementById("numberOfRepetitions").value;
-var e = document.getElementById("durationOfRepetitions").value;
+// Saving to local storage
+submit.addEventListener("click", saveWorkoutLocal); 
+const workoutHistory = [];  // An array that will contain the workout objects
+var tracker = 1;
 
+function saveWorkoutLocal() {
+  const a = document.getElementById("exerciseCategory").value;
+  const b = document.getElementById("exerciseName").value;
+  const c = document.getElementById("exerciseDifficulty").value;
+  const d = document.getElementById("numberOfRepetitions").value;
+  const e = document.getElementById("durationOfRepetitions").value;
 
-function printFormValue() {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-  console.log(d);
-  console.log(e);
+  const workout = {
+    category: a,
+    name: b,
+    difficulty: c,
+    number: d,
+    duration: e,
+  };
+
+  workoutHistory.push(workout); // Add the recent workout object into the workoutHistory array
+  localStorage.setItem('workout' + tracker, JSON.stringify(workout));
+  tracker++; // This should save each workout under a different number. For example: "workout1", "workout2", "workout3"
 }
 
-//var workout = [];
+
+
+// Retrieving from local storage
+function retrieveWorkoutLocal() {
+
+  if (workoutHistory.length > 0) { // If there is any element in the workoutHistory Array, process and loop through the array 
+    let i = 0;
+    while (i < workoutHistory.length) {
+      var workoutInfo = JSON.parse(localStorage.getItem('workout'));
+      console.log(workoutInfo);
+    }
+  } else { // If the array is empty, that means there is only one entry
+    var workoutInfo = JSON.parse(localStorage.getItem('workout'));
+    console.log(workoutInfo);
+  }
+}
+
+// innerHTML + retriving object onto spans similar to seconds in timer function
